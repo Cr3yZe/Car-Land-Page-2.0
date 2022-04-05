0e16d8eccf9703ad59882e1893f507e0ae015524
Cr3yZe <addy_czr@yahoo.com>
2022-04-05 11:43:55 +0300

    styling FAQ

---
+++ apps/ourTeamNav.js
@@ -0,0 +1,42 @@
+const allTeam = document.getElementsByClassName('person');
+const btnContainer = document.querySelector('.back-forth-container');
+console.log(btnContainer);
+
+let lastIndex = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 1; i < allTeam.length; i++){
+        allTeam[i].style.display = 'none';
+    }    
+});
+
+btnContainer.addEventListener('click', (ev) => {
+    let target = ev.target;
+
+    if (target.classList.contains('bx-left-arrow-alt')) {
+        lastIndex -= 1;
+        
+        if (lastIndex === -1){
+            lastIndex = 4;
+        }
+    } else if (target.classList.contains('bx-right-arrow-alt')) {
+        lastIndex += 1;
+
+        if (lastIndex === 5) {
+            lastIndex = 0;
+        }
+    }
+
+    lastIndex = lastIndex;
+
+    for (let i = 0; i < allTeam.length; i++){
+        allTeam[i].style.display = 'none';
+    }
+    
+    console.log(lastIndex);
+
+    for (let i = 0; i < allTeam.length; i++){
+        allTeam[lastIndex].style.display = 'grid';
+    }
+
+});
